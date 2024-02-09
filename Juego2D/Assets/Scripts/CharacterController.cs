0996a3025a692db437609d90947fca036988f11d
Jonasito92 <131693851+Jonasito92@users.noreply.github.com>
2024-02-09 17:42:27 +0100

    Animaciones y Scripts
    Animaciones del personaje, scripts de movimiento, salto etc

--- Juego2D/Assets/Scripts/CharacterController.cs
+++ Juego2D/Assets/Scripts/CharacterController.cs
@@ -7,21 +7,52 @@ public class NewBehaviourScript : MonoBehaviour
     public float speed;
     Rigidbody2D rigidBody;
     private float inputMovement;
-    public bool isLookingRight = true;
+    public bool isLookingRight = true, isOnFloor = false;
+    public float jumpForce;
+    private BoxCollider2D boxCollider;
+    public LayerMask surfaceLayer;
+    //Animator
+    public bool isRunning;
+    Animator animator;
+
+
+
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
     {
         ProcessingMovement();
+        ProcessingJump();
+        isOnFloor = CheckingFloor();
+
     }
+    bool CheckingFloor()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(
+                                        boxCollider.bounds.center, //Origen de la caja
+                                        new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), //Tamaño
+                                        0f, //Ángulo
+                                        Vector2.down, //Direccion hacia la que va la caja
+                                        0.2f, //Distancia a la que aparece la caja
+                                        surfaceLayer//Layer mask
+                                        );
+        return raycastHit.collider != null; //Devuelvo un valor siempre que no sea nulo
+    }
+
     void ProcessingMovement()
     {
         //Movement logic
         inputMovement = Input.GetAxis("Horizontal");
+        //Animation
+        isRunning = inputMovement != 0 ? true : false;
+        animator.SetBool("isRunning", inputMovement != 0);
+
         rigidBody.velocity = new Vector2(inputMovement * speed, rigidBody.velocity.y);
         CharacterOrientation(inputMovement);
 
@@ -37,5 +68,14 @@ public class NewBehaviourScript : MonoBehaviour
 
 
     }
+    void ProcessingJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnFloor)
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+    }
+
 }
 
